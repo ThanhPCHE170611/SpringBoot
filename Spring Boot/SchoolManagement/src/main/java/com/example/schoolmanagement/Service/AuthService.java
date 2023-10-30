2a495dfa9e3592c371f47ae3bcdbe1b43943a5db
thanhhack1234 <tpc23072003@gmail.com>
2023-10-30 14:21:49 +0700

    add logic for change password in 6 month use

---
+++ Spring Boot/SchoolManagement/src/main/java/com/example/schoolmanagement/Service/AuthService.java
@@ -0,0 +1,22 @@
+package com.example.schoolmanagement.Service;
+
+import com.example.schoolmanagement.Model.Users;
+import com.example.schoolmanagement.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void updatePassword(Users user) {
+        Users userInDb = userRepository.findUsersByUsername(user.getUsername()).get();
+        userInDb.setLastchangepassword(user.getLastchangepassword());
+        userInDb.setPassword(user.getPassword());
+    }
+}
