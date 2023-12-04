1d1d4b5b876982153218c44460cca94f9fd1c1d8
thanhhack1234 <tpc23072003@gmail.com>
2023-12-04 17:10:09 +0700

    front-end for OTP validation

--- SchoolManagement/src/main/java/com/example/schoolmanagement/SecurityConfig/SecurityConfig.java
+++ SchoolManagement/src/main/java/com/example/schoolmanagement/SecurityConfig/SecurityConfig.java
@@ -6,18 +6,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
@@ -50,7 +43,7 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/auth/login")
                 .failureUrl("/auth/login?error=true")
-                .defaultSuccessUrl("/user/welcome")
+                .defaultSuccessUrl("/otpvalidate")
                 .permitAll()
                 .and()
                 .csrf().disable()
